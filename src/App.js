e87b618bec08e5d4504798230947cbbfbd39f0f6
Juan Ponce <me.juanponce@gmail.com>
2019-12-13 19:30:28 -0600

    Html structure (header, hero, about_us, services, projects, exclusive_content, values, testimonials and news)

--- src/App.js
+++ src/App.js
@@ -5,10 +5,168 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>TEST</h1> 
+      <header className="header">
+        <img src="" alt="logo"/>
+        <nav className="nav">
+          <ul>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Portfolio</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </nav>
       </header>
-    </div>
+      <div className="hero">
+        <h1>Transforming Companies</h1>
+        <p>The path for the future</p>
+        <div className="hero_btn">
+          <i className="icon"></i>
+        </div>
+      </div>
+      <div className="about_us">
+        <h2>About Studio</h2>
+        <div className="about_content">
+          <div className="about_item">
+            <p>It is not the strongest of the species that survives, nor the most intelligent that survives. It is the one that is most adaptable to change.</p>
+            <p>– Charles Darwin</p>
+          </div>
+          <div className="about_item">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
+          <div className="about_item">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
+        </div>
+      </div>
+      <div className="services">
+        <h2>Services</h2>
+        <div className="services_wrapperItem">
+          <div className="services_item">
+            <i></i>
+            <p>Legacy Code</p>
+          </div>
+          <div className="services_item">
+            <i></i>
+            <p>Development</p>
+          </div>
+          <div className="services_item">
+            <i></i>
+            <p>Software</p>
+          </div>
+          <div className="services_item">
+            <i></i>
+            <p>Outsourcing</p>
+          </div>
+          <div className="services_item">
+            <i></i>
+            <p>Mobile</p>
+          </div>
+        </div>
+      </div>
+      <div className="projects">
+        <h2>Projects</h2>
+        <div className="projects_wrapperContent">
+          <div className="projects_left">
+            <div className="project_nav"></div>
+            <figure className="project_gallery">
+              <img src="" alt="img"/>
+              <img src="" alt="img"/>
+              <img src="" alt="img"/>
+              <img src="" alt="img"/>
+              <img src="" alt="img"/>
+            </figure>
+          </div>
+          <div className="projects_right">
+            <h3>A Starry Night</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            <a href="#">View Project</a>
+          </div>
+        </div>
+      </div>
+      <div className="exclusive_content">
+        <div className="exclusive_content_leftWrapper">
+          <h2>Looking for exclusive content?</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <a href="#">Contact Us</a>
+        </div>
+        <img src="" alt="img"/>
+      </div>
+      <div className="values">
+        <h2>Why choose us?</h2>
+        <div className="values_itemsWrapper">
+          <div className="values_item">
+            <i></i>
+            <h3>We're responsable</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
+          </div>
+          <div className="values_item">
+            <i></i>
+            <h3>We're creative</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
+          </div>
+          <div className="values_item">
+            <i></i>
+            <h3>We listen our community</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
+          </div>
+          <div className="values_item">
+            <i></i>
+            <h3>We love minimalism</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
+          </div>
+          <div className="values_item">
+            <i></i>
+            <h3>We have magic</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
+          </div>
+          <div className="values_item">
+            <i></i>
+            <h3>We're punctual</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
+          </div>
+        </div>
+      </div>
+      <div className="testimonials">
+        <h2>Testimonials</h2>
+        <div className="testimonials_itemsWrapper">
+          <div className="testimonials_items">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
+            <i className="testimonial_item_arrow"></i>
+            <i className="testimonial_item_arrow"></i>
+          </div>
+          <div className="testimonial_users">
+            <div className="testimonial_username">
+              <p>Barry Allen</p>
+              <p>Cisco Ramon</p>
+              <p>Ralph Diphny</p>
+            </div>
+            <div className="testimonial_navbar">
+              <div className="testimonial_bar"></div>
+              <div className="testimonial_bar"></div>
+              <div className="testimonial_bar"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="news">
+        <h2>News</h2>
+        <div className="news_itemsWrapper">
+          <div className="news_post"></div>
+          <div className="news_post"></div>
+          <div className="news_post"></div>
+        </div>
+      </div>
+      <div className="newsletter">
+        <h2>Newsletter</h2>
+        <form action="" method="POST">
+          <input className="newsletter_emailInput" type="email" placeholder="Enter your email"/>
+          <input className="newsletter_btn" type="submit"/>
+        </form>
+      </div>
+  </div>
   );
 }
 
