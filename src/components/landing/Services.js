c180906d86ceb2d00c69ae593327e74d76116856
Juan Ponce <me.juanponce@gmail.com>
2020-01-07 14:22:35 -0600

    Services Update

--- src/components/landing/Services.js
+++ src/components/landing/Services.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Services = () => {
   const options = [
-    { title: 'Legacy Code', icon: 'icon-align-right' },
-    { title: 'Custom Software', icon: 'icon-document-code' },
-    { title: 'Outsourcing', icon: 'icon-flow-switch' },
-    { title: 'Mobile', icon: 'icon-phone' },
+    { title: 'Legacy Code', icon: 'a' },
+    { title: 'Custom Software', icon: 'b' },
+    { title: 'Outsourcing', icon: 'c' },
+    { title: 'Mobile', icon: 'd' },
   ];
 
   return (
@@ -17,8 +17,8 @@ const Services = () => {
         <div className="services_menu flex-ai-center white">
           {options.map(option => {
             return (
-              <div key={option.title} className="flex flex-d-col flex-ai-center service_item zoom-in-bounce">
-                <i className={`${option.icon} service_icon`}></i>
+              <div key={option.title} className={`flex flex-d-col flex-ai-center service_item zoom-in-bounce ${option.icon}`}>
+                {/* <i className={`${option.icon} service_icon`}></i> */}
                 <p className="service_title">{option.title}</p>
               </div>
             );
