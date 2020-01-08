157f58320e64dd62e24684ebe36b81390b2b196d
Juan Ponce <me.juanponce@gmail.com>
2020-01-08 09:24:51 -0600

    test

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import Legacy_code from "./components/Services/legacy_code"
 import Custom_software from "./components/Services/custom_software"
 import Outsourcing from "./components/Services/outsourcing"
 import Mobile from "./components/Services/mobile"
-import Main_home from "./components/landing/Main"
+import Main from './components/landing/Main';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route exact path="/" component={Main_home}/>
+        <Main/>
         <Route path="/legacy" component={Legacy_code}/>
         <Route path="/software" component={Custom_software}/>
         <Route path="/outsourcing" component={Outsourcing}/>
