4c9ec312611472136badd3d681b0a3f43451e2cc
Miguel <miguelzamudio.dev@hotmail.com>
2020-01-16 21:12:02 -0700

    Add webmail login

---
+++ src/components/basic/WebMail.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class WebMail extends Component {
+  componentDidMount() {
+    window.location.href = "https://p3plcpnl0745.prod.phx3.secureserver.net:2096/logout/?locale=en";
+  }
+}
+
+export default WebMail;
