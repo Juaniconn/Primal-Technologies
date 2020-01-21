74b708514fafa6e29854de17dd1ed5d061ae226b
Miguel <miguelzamudio.dev@hotmail.com>
2020-01-21 15:47:34 -0700

    Add Switcher for routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/landing/Main';
 import Legacy_code from "./components/Services/legacy_code"
 import Custom_software from "./components/Services/custom_software"
@@ -10,15 +10,18 @@ import WebMail from './components/basic/WebMail';
 
 function App() {
   return (
-    <Router>
-      <Route exact path="/" component={Home} />
-      <Route path="/legacy" component={Legacy_code} />
-      <Route path="/software" component={Custom_software} />
-      <Route path="/outsourcing" component={Outsourcing} />
-      <Route path="/mobile" component={Mobile} />
-      <Route path="/projects" component={Projects} />
-      <Route path="/webmail" component={WebMail} />
-    </Router>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/legacy" component={Legacy_code} />
+        <Route path="/software" component={Custom_software} />
+        <Route path="/outsourcing" component={Outsourcing} />
+        <Route path="/mobile" component={Mobile} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/webmail" component={WebMail} />
+      </Switch>
+    </BrowserRouter>
+
   );
 }
 
